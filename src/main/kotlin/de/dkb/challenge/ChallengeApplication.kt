caa70ed4041ccffca31301fa07d0d90ea68a8e03
Coding Wookiee <thereal.coding.wookiee@gmail.com>
2025-01-16 21:27:12 +0100

    git init

---
+++ src/main/kotlin/de/dkb/challenge/ChallengeApplication.kt
@@ -0,0 +1,11 @@
+package de.dkb.challenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChallengeApplication>(*args)
+}
