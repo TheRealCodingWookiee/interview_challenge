e62245abb979afff5ef9bdaa23417cacb4b39435
Coding Wookiee <thereal.coding.wookiee@gmail.com>
2025-01-17 08:24:45 +0100

    add libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,9 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.13.7")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.27.0")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
