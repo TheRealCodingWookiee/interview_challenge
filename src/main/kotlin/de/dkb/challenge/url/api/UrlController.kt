c7424953ec8c11198400a059eaef2a2896ab6a12
Coding Wookiee <thereal.coding.wookiee@gmail.com>
2025-01-17 09:19:48 +0100

    implement controller and exception h andler

--- src/main/kotlin/de/dkb/challenge/url/api/UrlController.kt
+++ src/main/kotlin/de/dkb/challenge/url/api/UrlController.kt
@@ -1,25 +1,39 @@
 package de.dkb.challenge.url.api
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import de.dkb.challenge.url.business.UrlInvalidException
+import de.dkb.challenge.url.business.UrlNotFoundException
+import de.dkb.challenge.url.business.UrlService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/url")
-class UrlController {
+class UrlController(
+    private val urlService: UrlService
+) {
 
-    @PostMapping
+    @PostMapping("/shorten")
     fun shortenUrl(
         //could be extracted to a dto data class for a cleaner seperation
         @RequestBody url: String
-    ) {
-        // TODO
+    ): ResponseEntity<String> {
+        val shortenedUrl = urlService.shortenUrl(url)
+        return ResponseEntity.ok(shortenedUrl)
     }
 
-    @GetMapping
-    fun getUrl() {
-        // TODO
+    @GetMapping("/{shortenedUrl}")
+    fun getUrl(@PathVariable shortenedUrl: String): ResponseEntity<String> {
+        val url = urlService.getUrl(shortenedUrl)
+        return ResponseEntity.ok(url)
+    }
+
+    @ExceptionHandler(UrlInvalidException::class)
+    fun handleUrlInvalidException(e: UrlInvalidException): ResponseEntity<String> {
+        return ResponseEntity.badRequest().body(e.message)
+    }
+
+    @ExceptionHandler(UrlNotFoundException::class)
+    fun handleUrlNotFoundException(e: UrlNotFoundException): ResponseEntity<String> {
+        return ResponseEntity.notFound().build()
     }
 }
