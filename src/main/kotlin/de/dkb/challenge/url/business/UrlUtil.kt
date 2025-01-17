dc83908b6c2aa9f044293dbf5b57bcd4e93b00aa
Coding Wookiee <thereal.coding.wookiee@gmail.com>
2025-01-17 09:23:43 +0100

    add anotation

--- src/main/kotlin/de/dkb/challenge/url/business/UrlUtil.kt
+++ src/main/kotlin/de/dkb/challenge/url/business/UrlUtil.kt
@@ -1,8 +1,10 @@
 package de.dkb.challenge.url.business
 
+import org.springframework.stereotype.Component
 import java.security.MessageDigest
 
 //extracted for better testability
+@Component
 class UrlUtil {
     fun hashUrl(url:String, length: Int = 6): String {
         val bytes = MessageDigest.getInstance("SHA-256").digest(url.toByteArray())
