f70e1fd0f0b881b50c590c46b1b1a2a38a571678
Coding Wookiee <thereal.coding.wookiee@gmail.com>
2025-01-17 09:19:54 +0100

    add tests

---
+++ src/test/kotlin/de/dkb/challenge/url/api/UrlControllerTest.kt
@@ -0,0 +1,46 @@
+package de.dkb.challenge.url.api
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import de.dkb.challenge.url.business.UrlService
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.OK
+
+@ExtendWith(MockKExtension::class)
+class UrlControllerTest {
+ @InjectMockKs lateinit var sut: UrlController
+ @RelaxedMockK lateinit var urlService: UrlService
+
+  @Test
+  fun `should return shorten url from url service` () {
+   val url = "www.google.com"
+   val expectedShortenUrl = "1234"
+
+   every { urlService.shortenUrl(url) } returns expectedShortenUrl
+
+   val response = sut.shortenUrl(url)
+
+   assertThat(response.statusCode).isEqualTo(OK)
+   assertThat(response.body).isEqualTo(expectedShortenUrl)
+  }
+
+  @Test
+  fun `should return url for given shorten url` () {
+   val shortenedUrl = "1234"
+   val expectedUrl = "www.google.de"
+
+   every { urlService.getUrl(shortenedUrl) } returns expectedUrl
+
+   val response = sut.getUrl(shortenedUrl)
+
+   assertThat(response.statusCode).isEqualTo(OK)
+   assertThat(response.body).isEqualTo(expectedUrl)
+  }
+ }
